e8ee0bb230f982a3f7d908ef18284ab2708379b5
RoyaShahroudi <royaShahroudi@github.com>
2021-04-05 20:50:50 +0430

    feat: add colors, fonts, container, manWidth, minHeight and line clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#10C5AF",
+        footer: "#009E8B",
+      },
+      fontFamily: {
+        hero: ["Train One", "ui-sans-serif"],
+        body: ["Libre Franklin"],
+        persian: ["Markazi Text"],
+      },
+      lineClamp: {
+        1: "1",
+        3: "3",
+      },
+      maxWidth: {
+        112: "28rem",
+      },
+      minHeight: {
+        cal: 'calc(100vh - 128px)',
+      }
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
+
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+
+  plugins: [require("@tailwindcss/line-clamp")],
+};
