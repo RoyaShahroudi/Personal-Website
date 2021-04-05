25b891a2d8ee2027b2326b74e3e415844ad4765e
RoyaShahroudi <royaShahroudi@github.com>
2021-04-05 20:58:54 +0430

    feat: add title and fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
-import '../styles/tailwind.css'
-import '../styles/globals.css'
+import Head from "next/head";
+
+import "../styles/tailwind.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Roya Shahroudi</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
