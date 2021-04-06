ac76b9790177deb0133c28435c0eabdd2ba59fa5
RoyaShahroudi <royaShahroudi@github.com>
2021-04-06 23:05:16 +0430

    feat: add mongodb

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,8 +1,30 @@
+import {useRef} from 'react';
+
 const ContactForm = () => {
+  const nameRef = useRef('');
+  const emailRef = useRef('');
+  const messageRef = useRef('');
+
+  const sendMessageHandler = (event) => {
+    event.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        message: messageRef.current.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
   return (
     <section className="bg-gray-100 py-12 grid place-content-center min-h-cal">
       <div className="container">
-        <div className="py-8 px-4 bg-white rounded-lg grid grid-cols-1 md:grid-cols-2">
+        <div className="py-8 px-4 bg-white rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2">
           <div className="grid place-content-center">
             <div>
               <h2 className="pb-2 text-gray-700 text-3xl font-bold tracking-widest">
@@ -12,11 +34,12 @@ const ContactForm = () => {
                 Feel free to contact me
               </h3>
             </div>
-            <form>
+            <form onSubmit={sendMessageHandler}>
               <div>
                 <input
                   type="text"
-                  name="name"
+                  ref={nameRef}
+                  required
                   placeholder="Name"
                   className="bg-gray-200 rounded-full placeholder-white focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 pl-3 w-50"
                 />
@@ -24,7 +47,8 @@ const ContactForm = () => {
               <div className="pt-4">
                 <input
                   type="email"
-                  name="email"
+                  ref={emailRef}
+                  required
                   placeholder="Email"
                   className="bg-gray-200 rounded-full placeholder-white focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 pl-3 w-50"
                 />
@@ -32,7 +56,8 @@ const ContactForm = () => {
               <div className="pt-4">
                 <textarea
                   type="text"
-                  name="message"
+                  ref={messageRef}
+                  required
                   placeholder="Message"
                   rows="5"
                   className="bg-gray-200 rounded-2xl placeholder-white focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 pl-3 w-50"
